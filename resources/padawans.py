3086147fd9e535ad4e62a794d809cebe5de362df
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-11-19 21:26:38 -0600

    user can successfully login

--- resources/padawans.py
+++ resources/padawans.py
@@ -35,3 +35,32 @@ def register():
         del padawan_dict['password']
 
         return jsonify(data=padawan_dict, status={"code": 201, "message": "Successfully registered {}".format(padawan_dict['full_name'])}), 201
+
+@padawans.route('/login', methods=['POST'])
+def login():
+  payload = request.get_json()
+
+  try:
+    # look up padawan by full_name
+    padawan = models.Padawan.get(models.Padawan.full_name == payload['full_name'])
+
+    # access info about padawan
+    padawan_dict = model_to_dict(padawan) 
+
+    # check padawan's password using bcrypt
+    if(check_password_hash(padawan_dict['password'], payload['password'])):
+
+      # how we actually log in the user
+      login_user(padawan) 
+
+      del padawan_dict['password']
+
+      return jsonify(data=padawan_dict, status={'code': 200, 'message': "Succesfully logged in {}".format(padawan_dict['full_name'])}), 200
+
+    else:
+      print('this password is no good')
+      return jsonify(data={}, status={'code': 401, 'message': 'Email or password is incorrect'}), 401
+
+  except models.DoesNotExist:
+    print('this email was not found')
+    return jsonify(data={}, status={'code': 401, 'message': 'Email or password is incorrect'}), 401
